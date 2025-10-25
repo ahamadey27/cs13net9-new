795b77116ddef1e83d1eee1b5c814fbe550d3af1
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-10-25 16:22:13 -0400

    Add solution file and project reference for unit tests
    Added a new Visual Studio solution file (`cs13net9-new.sln`)
    with Debug and Release configurations targeting "Any CPU".
    Updated `CalculatorLibUnitTests.csproj` to include a project
    reference to `CalculatorLib.csproj`, enabling the unit test
    project to access and test the main library's functionality.

---
+++ chapter-04/CalculatorLibUnitTests/CalculatorUnitTests.cs
@@ -0,0 +1,12 @@
+﻿using C:\Users\hamad\OneDrive\Documentos\GitHub\cs13net9-new\chapter - 04\CalculatorLib; 
+
+namespace CalculatorLibUnitTests;
+
+public class CalculatorUnitTests
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
