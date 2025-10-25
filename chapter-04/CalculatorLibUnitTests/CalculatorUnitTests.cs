1213427e37bd96f70409d1a7d75119c0480efdd1
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-10-25 16:29:37 -0400

    Refactor Calculator unit test for clarity and setup
    Updated the `using` directive to directly reference `CalculatorLib`.
    Renamed the namespace to `CalculatorLibUnitTests` for consistency.
    Renamed the test method to `TestAdding2And2` for better clarity.
    Added an Arrange step to the test method to define inputs and
    initialize the `Calculator` instance, improving test readability.

--- chapter-04/CalculatorLibUnitTests/CalculatorUnitTests.cs
+++ chapter-04/CalculatorLibUnitTests/CalculatorUnitTests.cs
@@ -1,12 +1,17 @@
-﻿using C:\Users\hamad\OneDrive\Documentos\GitHub\cs13net9-new\chapter - 04\CalculatorLib; 
+﻿using CalculatorLib;
 
 namespace CalculatorLibUnitTests;
 
 public class CalculatorUnitTests
 {
     [Fact]
-    public void Test1()
+    public void TestAdding2And2()
     {
 
+        //Arrange: Set up the inputs and unites under test
+        double a = 2;
+        double b = 2;
+        double expected = 4;
+        Calulator calc = new();
     }
 }
