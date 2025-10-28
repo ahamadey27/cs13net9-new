863fedf840e72534e95ae50e368e713048cba9c7
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-10-28 19:15:40 -0400

    Add ConfigureConsole method and fix syntax error
    Added a `ConfigureConsole` method to configure console output
    encoding, set application culture, and optionally display the
    current culture. Introduced a `using` directive for
    `System.Globalization` to support culture-related functionality.
    Fixed a missing semicolon in the `WriteLine` statement within
    the `if (showCulture)` block.

---
+++ chapter-05/PacktLibraryNet2/PeopleApp/Program.Helpers.cs
@@ -0,0 +1,21 @@
+﻿using Sytem.Globalization; 
+
+partial class Program
+{
+	private static void ConfigureConsole(
+		string culture = "en-us",
+		bool userComputerCulture = false,
+		bool showCulture = true)
+	{
+		OutputEncoding = System.Text.Encoding.UTF8;
+
+		if (!userComputerCulture )
+		{
+			CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo(culture);
+		}
+		if (showCulture)
+		{
+			WriteLine($"Current Culture: {CultureInfo.CurrentCulture.DisplayName}.")
+		}
+	}
+}
